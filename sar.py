8f170bb674db961a10efd3680b529868487c53d0
Kyohei Nakamura <nakamura.kyohei.lab@gmail.com>
2017-04-17 21:58:25 +0900

    Available multi servers

--- sar.py
+++ sar.py
@@ -1,42 +1,65 @@
-import pandas as pd
-import datetime as dt
 import sys
-import subprocess
+import os
+from datetime import datetime as dt
 import csv
-import socket
+import pandas as pd
+import subprocess
+
+import time
 
 # check parameter
 if(len(sys.argv) != 3):
-  print 'Usage: $ python %s filename cpu_num' % sys.argv[0]
+  print 'Usage: $ python %s server_list timestamp_list' % sys.argv[0]
   quit()
 
-# get parameter
-FILENAME=sys.argv[1]
-CPU_NUM=sys.argv[2]
+SERVERS=sys.argv[1]
+TIMESTAMPS=sys.argv[2]
 
-# open file (get writer)
-df = pd.read_csv(FILENAME)
-ofile = open('%s_cpu%s_info_%s.csv' % (socket.gethostname(), CPU_NUM, dt.datetime.now().strftime('%Y%m%d%H%M%S')), 'a')
-writer = csv.writer(ofile)
+# read input files
+df_sv = pd.read_csv(SERVERS)
+df_ts = pd.read_csv(TIMESTAMPS)
 
-# iterate row
-for key, row in df.iterrows():
-  # get column
-  header1 = row['header1']
-  header2 = row['header2']
-  starttime = dt.datetime.strptime(str(row['time1']), '%Y%m%d%H%M%S')
-  endtime = dt.datetime.strptime(str(row['time2']), '%Y%m%d%H%M%S')
-  outputList = [header1, header2, starttime.strftime('%Y/%m/%d %H:%M:%S'), endtime.strftime('%Y/%m/%d %H:%M:%S')]
+dirname = 'result_%s' % dt.now().strftime('%Y%m%d%H%M%S')
+os.mkdir(dirname)
 
-  try:
-    # execute sar
-    output = subprocess.check_output('sar -P %s -f /var/log/sysstat/sa%s -s %s -e %s 2>&1 | grep "^Average:"' % (CPU_NUM, starttime.strftime('%d'), starttime.strftime('%H:%M:%S'), endtime.strftime('%H:%M:%S')), shell=True)
-    outputList.extend(output.split()[2:])
-  except:
-    outputList.append('N/A')
+# iterate servers list
+for index_sv, row_sv in df_sv.iterrows():
+  hostname = row_sv['hostname']
+  cpunum = row_sv['cpunum']
 
-  # output csv
-  writer.writerow(outputList)
+  # open output file
+  ofile = open('./%s/%s_cpu%s_info_%s.csv' % ( \
+    dirname, \
+    hostname, \
+    cpunum, \
+    dt.now().strftime('%Y%m%d%H%M%S')), 'a')
+
+  writer = csv.writer(ofile)
+
+  # iterate timestamps list
+  for index_ts, row_ts in df_ts.iterrows():
+    starttime = dt.strptime(str(row_ts['starttime']), '%Y%m%d%H%M%S')
+    endtime = dt.strptime(str(row_ts['endtime']), '%Y%m%d%H%M%S')
+    output_list = list(row_ts)
+
+    try:
+      # execute sar
+      output = subprocess.check_output( \
+        'ssh %s sar -P %s -f /var/log/sysstat/sa%s -s %s -e %s 2>&1 | grep "^Average:"' % ( \
+          hostname, \
+          cpunum, \
+          starttime.strftime('%d'), \
+          starttime.strftime('%H:%M:%S'), \
+          endtime.strftime('%H:%M:%S')), shell=True)
+
+      output_list.extend(output.split()[2:])
+    except:
+      output_list.append('N/A')
+
+    # output csv
+    writer.writerow(output_list)
+
+  time.sleep(1)
 
 # close file
 ofile.close()
